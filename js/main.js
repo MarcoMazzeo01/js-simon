2bd7643ffa2521cc44fff5512a8c01855c1537f7
plastikSun <marcom2k01@gmail.com>
2023-08-23 16:42:27 +0200

    add: correct days and hours countdown

--- js/main.js
+++ js/main.js
@@ -3,6 +3,28 @@ const hoursCount = document.getElementById("hours");
 const minutesCount = document.getElementById("minutes");
 const secondsCount = document.getElementById("seconds");
 
-const todayDate = new Date();
 const goalDate = new Date(2023, 7, 24, 9, 30, 0, 0);
 const goalMs = goalDate.getTime();
+
+const msSec = 1000
+const msMin = 60000
+const msHour = 3600000
+const msDay = 86400000
+
+
+function check() {
+    
+    const msDiff = goalMs - (new Date().getTime())
+    console.log(msDiff)
+
+    daysCount.innerHTML = Math.round(msDiff / msDay)
+    hoursCount.innerHTML = Math.round(msDiff / msHour)
+    minutesCount.innerHTML = Math.round((msDiff / msMin)) 
+    secondsCount.innerHTML = Math.round((msDiff / msSec)) 
+
+
+    setTimeout(check,1000)
+}
+
+check()
+
